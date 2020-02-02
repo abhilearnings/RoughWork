b0303a41d382d441ea66b5b1e64821496690e0a3
abhi <abhilearnings2005@gmail.com>
2020-02-02 11:54:35 -0500

    done with today changes for payment page

---
+++ RoughWork/src/test/com/subclass2.java
@@ -0,0 +1,13 @@
+package test.com;
+
+public class subclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticMethodTest obj3 = new StaticMethodTest();
+		//obj1.i = 2;
+		System.out.println(obj3.i);
+
+	}
+
+}
